3a5af49e66d36361a4021b72935b08e99bb1ab56
pochat <mariopochat@gmail.com>
2024-04-27 15:09:27 -0700

    testing lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,19 @@
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
-  // your custom flat configs go here, for example:
-  // {
-  //   files: ['**/*.ts', '**/*.tsx'],
-  //   rules: {
-  //     'no-console': 'off' // allow console.log in TypeScript files
-  //   }
-  // },
-  // {
-  //   ...
-  // }
-)
+  {
+    // your custom flat configs go here, for example:
+    // {
+    //   files: ['**/*.ts', '**/*.tsx'],
+    //   rules: {
+    //     'no-console': 'off' // allow console.log in TypeScript files
+    //   }
+    // },
+    // {
+    //   ...
+    // }
+    rules: {
+      camelcase: ['error', { properties: 'always' }]
+    }
+  }
+);
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,15 @@
 
 const allgood = 5;
 
+
 </script>
 
 <template>
   <h1 class="mt-20">
     This is the index page
+ 
+    {{ allgood }}
+
   </h1>
   
 </template>
