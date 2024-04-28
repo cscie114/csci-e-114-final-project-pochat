74ce7eee6ca1d14f3dbd258dff938b6340ba661f
pochat <mariopochat@gmail.com>
2024-04-28 10:55:12 -0700

    output/server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   ],
 
   build: {
-    ssr: false // Enables Static Site Generation (SSG)
+    ssr: true // Enables Static Site Generation (SSG)
   },
 
   runtimeConfig: {
