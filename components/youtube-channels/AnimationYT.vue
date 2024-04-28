f3640c6eec3559f2bffb72a653e37dc371e66476
pochat <mariopochat@gmail.com>
2024-04-28 12:07:16 -0700

    redid the api in github

--- components/youtube-channels/AnimationYT.vue
+++ components/youtube-channels/AnimationYT.vue
@@ -10,7 +10,7 @@ const fetchData = async () => {
     console.log("log inside try: YOUTUBE_API", YOUTUBE_API);
     const response = await fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUQeRaTukNYft1_6AZPACnog&key=${YOUTUBE_API}`);
     if (!response.ok) {
-      throw new Error('Failed to fetch data');
+      throw new Error('Failed to fetch data from api');
     }
     const data = await response.json();
     console.log('data', data);
