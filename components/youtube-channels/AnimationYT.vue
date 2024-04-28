3229f79b637828cd580d2260b9ad0a8c84e76d18
pochat <mariopochat@gmail.com>
2024-04-28 09:24:35 -0700

    hardcoding api to test deploy

--- components/youtube-channels/AnimationYT.vue
+++ components/youtube-channels/AnimationYT.vue
@@ -7,7 +7,7 @@ const config = useRuntimeConfig();
 
     try {
       console.log("config.youTubeApi", config.youTubeApi);
-      const response = await fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUQeRaTukNYft1_6AZPACnog&key=${config.public.youTubeApi}`);
+      const response = await fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUQeRaTukNYft1_6AZPACnog&key=AIzaSyBabGf2NG8T52Y5b29WU6sU8OCkTk2vTwQ`);
       if (!response.ok) {
         throw new Error('Failed to fetch data');
       }
