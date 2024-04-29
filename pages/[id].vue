44fe0466e0772ce062f83243ca8f4b09997be267
pochat <48400262+pochat@users.noreply.github.com>
2024-04-29 16:19:27 -0700

    trying props

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,36 +1,39 @@
 <script setup>
 import { useRuntimeConfig } from "nuxt/app";
 
-const { id } = useRoute().params
+// Access route params
+const { id, playlistId } = useRoute().params;
 
-// Embed video from youtube
+console.log("playlistId:", playlistId);
+
+// Embed video from YouTube
 const videoUrl = `https://www.youtube.com/embed/${id}`;
 
-// Access values in nuxt.config.ts
+// Access values from nuxt.config.ts
 const config = useRuntimeConfig();
 
-//YOUTUBE_API is stored in github actions
-const YOUTUBE_API = config.public.youTubeApi; 
+// YouTube API key
+const YOUTUBE_API = config.public.youTubeApi;
 
+// Fetch YouTube data
 const fetchData = async () => {
   try {
-    console.log("log inside try: YOUTUBE_API", YOUTUBE_API);
     const response = await fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUrcg40cuUt7QZFUd-cOywPw&videoId=${id}&key=${YOUTUBE_API}`);
     if (!response.ok) {
-      throw new Error('Failed to fetch data from api');
+      throw new Error('Failed to fetch data from API');
     }
     const data = await response.json();
-    console.log('data', data);
     return data;
   } catch (error) {
     console.error(error);
-    return null; 
+    return null;
   }
 };
 
 const youTubeData = await fetchData();
-console.log('youTubeData from [id]', youTubeData);
 </script>
+
+
 <template>
   <div class="mt-10 justify-center bg-orange-100">
     <div class="flex">
@@ -52,7 +55,10 @@ console.log('youTubeData from [id]', youTubeData);
 
       </div>
       <div class="w-1/3 bg-slate-600">
-        Right div
+        Right div:holas  {{ playlistId }}
+        <p>Received Playlist ID: {{ playlistId }}</p>
+        <p>Debug Information:</p>
+        <pre>{{ $route.params }}</pre>
       </div>
     </div>
   </div>
